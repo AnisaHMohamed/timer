64c175a3aa30b33888cab3642006b403809e0c52
Anisa <anisahamohamed@gmail.com>
2019-12-03 20:43:40 +0000

    added timer1.js with edits to not accept numbers and negatives

--- timer1.js
+++ timer1.js
@@ -1,13 +1,34 @@
-function sortNumber(a, b) {
-  return a - b;
-}
+// function sortNumber(a, b) {
+//   return a - b;
+// }
+
+// const args =  process.argv.slice(2)
+// //console.log(args.sort(sortNumber))
+
+// const sortedArgs = args.sort(sortNumber)
+// for (let i =0;i<sortedArgs.length;i++){
+  
+//   setTimeout(() => {process.stdout.write('\x07');
+
+// }, sortedArgs[i]*1000);
+// }
+// function sortNumber(a, b) {
+//   return a - b;
+// }
 
 const args =  process.argv.slice(2)
 //console.log(args.sort(sortNumber))
-
+function sortNumber(a, b) {
+  return a - b;
+}
 const sortedArgs = args.sort(sortNumber)
 for (let i =0;i<sortedArgs.length;i++){
-  setTimeout(() => {process.stdout.write('\x07');
+  if (!isNaN(sortedArgs[i]) && sortedArgs[i]>0 ){
 
+  setTimeout(() => {process.stdout.write('\x07');
+//console.log(sortedArgs[i])
 }, sortedArgs[i]*1000);
+}
+
+
 }
